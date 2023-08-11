eb56c95805d9bbfd4a43d1e422a96081d261cd70
Rui Matos <rmsmatos@gmail.com>
2023-08-11 20:07:38 +0100

    fixes

--- outputs.tf
+++ outputs.tf
@@ -7,28 +7,28 @@ output "location" {
   description = "Returns Resource Group Location."
 }
 output "name" {
-  value       = module.azurerm_resourcegroup_type1.azurecvt_resourcegroup.name
+  value       = module.azurerm_resourcegroup_type1.name
   description = "Returns Resource Group Name."
 }
 output "tags" {
   value       = module.azurerm_resourcegroup_type1.tags
   description = "Returns Resource Group Tags."
 }
-output "managed_by" {
-  value       = module.azurerm_resourcegroup_type1.managed_by
-  description = "Returns the ID of the resource or application that manages this Resource Group."
-}
+#output "managed_by" {
+#  value       = module.azurerm_resourcegroup_type1.managed_by
+#  description = "Returns the ID of the resource or application that manages this Resource Group."
+#}
 
 output "lock_id" {
   value       = resource.azurerm_management_lock.lock.id
   description = "Returns Resource Group Lock ID."
 }
 output "lock_name" {
-  value       = resource.azurerm_management_lock.name
+  value       = resource.azurerm_management_lock.lock_name
   description = "Returns Resource Group Lock Name."
 }
 output "lock_scope" {
-  value       = resource.azurerm_management_lock.scope
+  value       = resource.azurerm_management_lock.lock_scope
   description = "Returns Resource Group Lock scope"
 }
 output "lock_level" {
@@ -36,6 +36,6 @@ output "lock_level" {
   description = "Returns Resource Group Lock Level"
 }
 output "lock_notes" {
-  value       = resource.azurerm_management_lock.notes
+  value       = resource.azurerm_management_lock.lock_notes
   description = "Returns Resource Group Lock Note."
 }
